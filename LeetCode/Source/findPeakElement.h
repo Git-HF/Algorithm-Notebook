c8d31df962790871c6680210c339a971e41c9bac
hf <1102627084@qq.com>
2020-08-03 22:44:46 +0800

    08_03 leetcode打卡

---
+++ LeetCode/Source/findPeakElement.h
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        if(nums.size() <= 1)
+            return 0;
+        
+        int left = 0, right = nums.size() - 1;
+        int mid;
+        while(left < right)
+        {
+            mid = (left + right) / 2;
+            if(nums[mid] < nums[mid+1])
+            {
+                left = mid+1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+};
